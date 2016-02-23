7c39e884433eb12dbd7b299fbcd01ddc2802b969
Mike Eirih <maokomioko@users.noreply.github.com>
2016-02-23 14:06:55 +0100

    fix for missing historical_date

--- lib/money/bank/currencylayer_bank.rb
+++ lib/money/bank/currencylayer_bank.rb
@@ -157,7 +157,7 @@ class Money
         cl_url = historical_date ? CL_HISTORICAL_URL : CL_URL
         cl_url = CL_SECURE_URL if secure_connection
 
-        "#{cl_url}?source=#{source}&access_key=#{access_key}#{"&date=#{historical_date}" if historical_date.present?}"
+        "#{cl_url}?source=#{source}&access_key=#{access_key}#{"&date=#{historical_date}" if historical_date}"
       end
 
       # Get the timestamp of rates
